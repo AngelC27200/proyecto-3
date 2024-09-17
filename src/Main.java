9b88966b93063c3334a6101318cfe1adeb154acf
AngelC27200 <langellopez81@gmail.com>
2024-09-17 17:00:39 -0600

    agrego el main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        List<Alumno> alumnos = Alumno.crearAlumnosEjemplo();
+        List<Materia> materias = Materia.crearMateriasPorDefecto();
+        List<Especialidad> especialidades = Especialidad.crearEspecialidadesEjemplo();
+        List<Catedraticos> catedraticos = Catedraticos.crearCatedraticosEjemplo();
+
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("\n--- Menú Principal ---");
+            System.out.println("1. Mostrar Alumnos");
+            System.out.println("2. Agregar Alumno");
+            System.out.println("3. Mostrar Materias");
+            System.out.println("4. Agregar Materia");
+            System.out.println("5. Mostrar Especialidades");
+            System.out.println("6. Agregar Especialidad");
+            System.out.println("7. Mostrar Catedráticos");
+            System.out.println("8. Agregar Catedrático");
+            System.out.println("9. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1 -> Alumno.mostrarAlumnos(alumnos);
+                case 2 -> {
+                    Alumno.agregarAlumno(alumnos, scanner);
+                    Alumno.mostrarAlumnos(alumnos);
+                }
+                case 3 -> Materia.mostrarMaterias(materias);
+                case 4 -> {
+                    Materia.agregarMateria(materias, scanner);
+                    Materia.mostrarMaterias(materias);
+                }
+                case 5 -> Especialidad.mostrarEspecialidades(especialidades);
+                case 6 -> {
+                    Especialidad.agregarEspecialidad(especialidades, scanner);
+                    Especialidad.mostrarEspecialidades(especialidades);
+                }
+                case 7 -> Catedraticos.mostrarCatedraticos(catedraticos);
+                case 8 -> {
+                    Catedraticos.agregarCatedratico(catedraticos, scanner);
+                    Catedraticos.mostrarCatedraticos(catedraticos);
+                }
+                case 9 -> System.out.println("Saliendo");
+                default -> System.out.println("Opción no válida.");
+            }
+        } while (opcion != 9);
     }
+
 }
